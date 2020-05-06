6c5b5664ffd5f37b8212147603979f4684dd5e52
jarvis <bogyum@icloud.com>
2020-05-06 11:21:35 +0900

    Do make project skeleton

---
+++ src/pyEncrypeUsingSHA256.py
@@ -0,0 +1,70 @@
+import logging, sys, os, re, hashlib
+import utilClass
+
+utils = utilClass.Utils()
+root = str(os.path.dirname(os.path.realpath(__file__)))
+
+def getEncrypeUsingSHA256(sourceFileName, saltFileName, keyColumes):
+
+    salt = ''
+    result = []
+    try:
+        logging.info("getEncrypeUsingSHA256() - Load salt")
+        with open(saltFileName, 'r') as saltFile:
+            salt = saltFile.read().replace('\n', '')
+        saltFile.close()
+
+        logging.info("getEncrypeUsingSHA256() - Do encryption")
+        with open(sourceFileName, 'r') as sourceFile:
+            while True:
+                line = sourceFile.readline()
+                if not line: break
+                data = line.split("\t")
+
+                keyString = data[keyColumes[0]] + data[keyColumes[1]] + data[keyColumes[2]]
+                result.append(hashlib.sha256( salt.encode() + keyString.encode()).hexdigest())
+        sourceFile.close()
+
+    except OSError:
+        logging.error("File open error")
+        exit()
+
+    return result;
+
+if __name__ == "__main__":
+    # Program argument setting
+    #   argument :: dev environment, crawling - date
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+
+    if len(sys.argv) != 4:
+        logging.error("argument error")
+        logging.error("  allowd argument :: (Encrype Source File) (Salt File) (Encrype Target File)")
+        logging.error("                     (Source.txt) (Encryped.txt) (Salt.txt) ")
+        exit()
+
+    # 환경 설정 파일 로딩
+    logging.info("main() - Load config file")
+    config = utils.readJsonFile(root + '/../config/config.json')
+
+    # 파라미터 로딩
+    logging.info("main() - Load arguments")
+    sourceFileName = root + '/..' + config['encrype_source']['file_path'] + '/' + sys.argv[1]
+    saltFileName = root + '/..' + config['encrype_source']['salt_path'] + '/' + sys.argv[2]
+    keyColumes = config['encrype_source']['key_columes']
+
+    # encrype using SHA256
+    logging.info("main() - Do encrype using sha256 with salt")
+    encrypeResult = getEncrypeUsingSHA256(sourceFileName, saltFileName, keyColumes)
+
+    # result print out
+    logging.info("main() - Result output")
+    targetFileName = root + '/..' + config['encrype_target']['file_path'] + '/' + sys.argv[3]
+    try:
+        with open(targetFileName, 'w') as outputFile:
+            for keys in encrypeResult:
+                outputFile.write(keys)
+                outputFile.write("\n")
+        outputFile.close()
+    except OSError:
+        logging.error("File write error :: " + targetFileName)
+
